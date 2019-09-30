dcb6750b8079d0cde60b6c4271edda0aa4c29ea5
Rebecca Holt <rebecca.holt9991@gmail.com>
2019-09-29 21:03:47 -0600

    wip

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let getCharacter = () => {
 
   if (char) {
     document.querySelector(".code").value = "";
+    document.querySelector("#name").innerText = char.name+" your character is: "
     document.querySelector("#character").innerText = char.character_name
     document.querySelector("#desc").innerText = char.desc
     hide.style.display = "inline";
@@ -20,6 +21,7 @@ let getCharacter = () => {
 }
 
 let hideCharacter = () => {
+  document.querySelector("#name").innerText = "";
   document.querySelector("#character").innerText = "";
   document.querySelector("#desc").innerText = "";
   document.querySelector(".code").value = "";
